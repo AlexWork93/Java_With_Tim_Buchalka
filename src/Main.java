f531b14beb6ce04eb40b721b7dfafdd51deb9e49
oleksii <oleksii.marianchenko@gmail.com>
2023-11-29 20:52:30 +0200

    completed challenge 126

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(findMin(readIntegers()));
+    }
 
+    public static int[] readIntegers(){
+        Scanner sc = new Scanner(System.in);
+        String[] string = sc.nextLine().split(",");
+        int[] intArr = new int[string.length];
+        for(int i = 0; i < intArr.length; i++){
+            intArr[i] = Integer.parseInt(string[i]);
+        }
+        return intArr;
+    }
+
+    public static int findMin(int[] intArr){
+        Arrays.sort(intArr);
+        return intArr[0];
     }
 }
